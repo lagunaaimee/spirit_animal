8eeffed9c1d975219c59a3437e1dbb956f99c751
Aimee <aimee.n.laguna@gmail.com>
2018-11-02 22:50:34 -0700

    created spirit_animaldb schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS spirit_animaldb;
+CREATE DATABASE spirit_animaldb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE spirit_animaldb;
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  userID VARCHAR(45) NOT NULL,
+  userImage VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  question VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE answers (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  answer INT NOT NULL
+);
+
+INSERT INTO users (userID, userImage)
+VALUES ("aimee", "https://www.tanyacasteel.com/wp-content/uploads/2017/04/phoenix-spirit-animal-small.jpeg");
