9e79598409398cb5f8bea2bf5146767367400e27
Benjamin Weinberger <benjaminweinberger@gmail.com>
2018-11-06 21:45:03 -0700

    lots of changes, mvp mostly done

--- models/questions.js
+++ models/questions.js
@@ -1,6 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var questions = sequelize.define("questions", {
-    question: DataTypes.STRING
+    question: DataTypes.STRING,
+    instructions: DataTypes.STRING
   });
+  questions.bulkCreate([
+    { question: "If you could be any animal, what would you be?" },
+    { question: "What's your favorite smell?" },
+    { question: "What's your go-to cheat meal?" },
+    { question: "What animal do you fear the most?" },
+    {
+      question:
+        "If you were in a horror film, what weapon would you use to defend yourself?"
+    },
+    { instructions: "Answer the following quesiton by uploading a photo."}
+  ]);
   return questions;
 };
