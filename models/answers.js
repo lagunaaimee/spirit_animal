f5f4079704f4b973deb7143d4ae2957f08455b55
marcus finch <marcuswilliam0104@gmail.com>
2018-11-08 20:19:01 -0700

    Answer Model Updated

--- models/answers.js
+++ models/answers.js
@@ -1,8 +1,19 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var answers = sequelize.define("answers", {
     userID: DataTypes.INTEGER,
-    questionID: DataTypes.INTEGER,
     answer: DataTypes.STRING
   });
+
+
+  
+  answers.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    answers.belongsTo(models.questions, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
   return answers;
 };
